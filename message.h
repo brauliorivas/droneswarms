79b6956e9a940fadc75d8e52657b17ec48a152e4
Braulio Rivas <brauliorivas2004@gmail.com>
2025-08-26 23:10:58 -0500

    format + delete file

--- message.h
+++ message.h
@@ -1,21 +1,26 @@
-#ifndef __ERROR_H__
-#define __ERROR_H__
+#ifndef __MESSAGE_H__
+#define __MESSAGE_H__
 
 #ifdef ERR_VERBOSE
-    #define print_error(namespace, msg) \
-        fprintf(stderr, "[%s] %s\n", namespace, msg)
-#else
-    #define print_error(namespace, msg) \
-        do {} while (0)
-#endif
+#define print_error(namespace, msg) fprintf(stderr, "[%s] %s\n", namespace, msg)
 
+#define vprint_error(namespace, fmt, ...)                                      \
+  fprintf(stderr, "[%s] " fmt "\n", namespace, ##__VA_ARGS__)
+#else
+#define print_error(namespace, msg)                                            \
+  do {                                                                         \
+  } while (0)
+#endif
 
 #ifdef LOG_VERBOSE
-    #define print_log(namespace, msg) \
-        fprintf(stdout, "[%s] %s\n", namespace, msg)
+#define print_log(namespace, msg) fprintf(stdout, "[%s] %s\n", namespace, msg)
+
+#define vprint_log(namespace, fmt, ...)                                        \
+  fprintf(stdout, "[%s] " fmt "\n", namespace, ##__VA_ARGS__)
 #else
-    #define print_log(namespace, msg) \
-        do {} while (0)
+#define print_log(namespace, msg)                                              \
+  do {                                                                         \
+  } while (0)
 #endif
 
-#endif // !__ERROR_H__
+#endif // !__MESSAGE_H__
